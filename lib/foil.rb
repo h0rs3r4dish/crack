2ca184cadf5b2519c1fad3196e626da67480d25f
Chris <kidkog@gmail.com>
2011-06-22 18:22:31 -0400

    Rewrote Foil because the old @map storage sucked. It's now, instead of a String, a one-dimensional array of Strings (@map[y] = characters on the x-axis)

--- lib/foil.rb
+++ lib/foil.rb
@@ -3,10 +3,9 @@ module Foil
 class Window
 	attr_reader :window_id, :dimensions
 
-	def initialize(x,y,w=80,h=24)
-		x += 1; y += 1
+	def initialize(x=0,y=0,w=80,h=24)
 		@dimensions = { :x => x, :y => y, :w => w, :h => h }
-		@map = " " * w * h + " " # First character is a throwaway
+		@map = Array.new(h) { " " * w }
 		@window_id = Foil.register_window self
 	end
 
@@ -14,79 +13,71 @@ class Window
 		Foil.hide_window self
 	end
 	def show
-		redraw @dimensions.values
+		redraw 0, 0, @dimensions[:h], @dimensions[:w]
 	end
 
 	def get_char_at(x,y)
-		@map[x * y]
+		@map[y][x]
 	end
 	def get_char_range(x,y,w,h)
-		x += 1; y += 1
-		(1..h).map { |hh|
-			@map[x*hh, (x+w)*hh]
+		(0..h).map { |hh|
+			@map[hh][x..(x+w)]
 		}
 	end
 
 	def redraw(x,y,w,h)
-		x += 1; y += 1
 		h.times { |hh|
 			yy = y + hh
-			text_at(x,y+hh, @map[x * yy, (x + w) * yy])
+			text_at(x, yy, @map[yy][x..(x+w)])
 		}
 	end
 		
-
 	def clear
-		cursor_to(1, 1)
-		@map = ""
-		@dimensions[:h].times {
-			spaces = " " * @dimensions[:w]
-			puts spaces
-			@map += spaces
+		@map = Array.new(@dimensions[:h]) { |y|
+			cursor_to 0, y
+			print (spaces = " " * @dimensions[:w])
+			spaces
 		}
 	end
 	def text_at(x, y, text)
-		x += 1; y += 1
-		perline = @dimensins[:w] - x
+		perline = @dimensions[:w] - x
 		text.split("\n").each { |line|
 			if line.length > perline then
 				loc = 0
 				while loc < line.length
 					cursor_to(x, y)
-					start = x * y
 					segment = line[loc, loc+perline]
 
-					puts segment
-					@map[start, start+perline] = segment
+					print segment
+					@map[y][loc..(loc+perline)] = segment
 
 					loc += perline
 					y += 1
 					return if y > @dimensions[:h]
 				end
 			else
-				cursor_to(x, y)
-				start = x * y
-
-				puts line
-				@map[start, start + line.length] = line
-
+				cursor_to x, y
+				print line
+				@map[y][x..(x + line.length)] = line
 				y += 1
 			end
-			return if cy > @dimensions[:h]
+			return if y > @dimensions[:h]
 		}
 	end
 	def char_at(x, y, c)
-		x += 1; y += 1
 		cursor_to(x, y)
-		@map[x * y] = c
 		print c
+		@map[y][x] = c
 	end
 
 	def cursor_up(n=1); print "\033[#{n}A"; end
 	def cursor_down(n=1); print "\033[#{n}B"; end
 	def cursor_right(n=1); print "\033[#{n}C"; end
 	def cursor_left(n=1); print "\033[#{n}D"; end
-	def cursor_to(l=1,c=1); print "\033[#{l-1};#{c-1}H"; end
+	def cursor_to(l=1,c=1)
+		l += @dimensions[:y] + 1; c += @dimensions[:x] + 1
+		print "\033[#{c};#{l}H"
+	end
 end
 
 class << self
@@ -103,7 +94,7 @@ class << self
 	def hide_window(w)
 		# FIXME: Right now, it redraws everything. Should just find the
 		# specific areas to redraw. later. Much later.
-		windows.reverse.each { |ww|
+		@window_list.reverse.each { |ww|
 			ww.show unless ww == w
 		}
 	end
