f6e280ba7f71e4d55b8942e17e3947b9da32e5dd
Chris <kidkog@gmail.com>
2011-06-23 20:16:29 -0400

    Wrote tests for the MapTile class, and renamed the Map class to Floor, since it's way more descriptive of what it actually is.

--- lib/map.rb
+++ lib/map.rb
@@ -1,3 +1,5 @@
+require "lib/eventlog"
+
 module Crack
 
 class MapTile
@@ -7,17 +9,65 @@ class MapTile
 		@items = Array.new
 		@type = type
 	end
+	def set_type(t); @type = t; end
 	def seen?; @seen; end
 	def add_item(i); @items << i; end
-	def pop_item(i); @items.slice(@items.index_of(i),1); end
+	def pop_item(i); @items.delete_at(@items.index(i)); return i; end
 end
 
-class Map
+class Floor
+	FLOOR_WIDTH  = 80
+	FLOOR_HEIGHT = 23
+
+	GENERATE_ROOM_PROBABILITY = 0.75
+
 	def initialize(w,h)
 		@map = Array.new(h) { Array.new(w) }
 	end
+
+
+	# Map generator stuff
 	def generate!
-		@map.each { |row| row.map { MapTile.new } }
+		Log.open "mapgen"
+		Log.puts "Initializing blank map"
+		@map.each { |row| row.map { MapTile.new } } # Reset the map
+		loc_stair_up = rand 6; loc_stair_down = rand 6; # What sector they're in
+		while loc_stair_up == loc_stair_down; loc_stair_down = rand 6; end
+		Log.puts "Stairs -- Up: #{loc_stairs_up} -- Down: #{loc_stairs_down}"
+		offset_x_factor = FLOOR_WIDTH / 3; offset_y_factor = FLOOR_WIDTH / 2;
+		cur_sector = 0
+		Log.puts "Starting sector generation"
+		3.times { |sector_x|
+			2.times { |sector_y|
+				Log.puts "Current sector: #{cur_sector}"
+				offset_x = offset_x_factor * sector_x
+				offset_y = offset_y_factor * sector_y
+				Log.puts "Tile offset: #{offset_x}x#{offset_y}"
+				if rand < GENERATE_ROOM_PROBABILITY then
+					generate_room(offset_x, offset_y)
+				else
+					generate_hallway(offset_x, offset_y)
+				end
+				generate_stair(:up, cur_sector) if cur_sector == loc_stair_up
+				genreate_stair(:down,cur_sector) if cur_sector == loc_stair_down
+					
+				cur_sector += 1
+			}
+		}
+		generate_sector_connections(rand 6) # Start at a random sector
+	end
+
+	def generate_room(offset_x, offset_y)
+		Log.puts "Generating a room"
+	end
+	def generate_hallway(offset_x, offset_y)
+		Log.puts "Generating a hallway"
+	end
+	def generate_stair(dir, sector)
+		Log.puts "Creating stairs #{dir} in sector #{sector}"
+	end
+	def generate_sector_connections(start_sector)
+		Log.puts "Connecting all sectors, starting at #{start_sector}"
 	end
 end
 
---
+++ test/map.rb
@@ -0,0 +1,26 @@
+require "lib/map"
+
+describe Crack::MapTile do
+	it "defaults to a blank, unseen wall" do
+		subject.seen?.should == false
+		subject.type.should == :wall
+		subject.items.length.should == 0
+	end
+	it "allows the setting of its type through its constructor", :floor do
+		subject.type.should == :floor
+	end
+	it "can accept items being dropped and taken off of it" do
+		items = [
+			mock({ :name => "Sword of Walrus Slaying" }),
+			mock({ :name => "Helm of Grue Detection" })
+		]
+
+		items.each { |item| subject.add_item item }
+		subject.items.length.should == 2
+
+		item = subject.pop_item items.first
+		item.should == items.first
+		subject.items.length.should == 1
+		subject.items.should == [ items.last ]
+	end
+end
